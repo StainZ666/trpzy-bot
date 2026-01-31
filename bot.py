5a4cd9920eea14d3660a1e3f10bdf7316bbb6cb3
StainZ666 <makethashibru@gmail.com>
2026-01-31 20:14:10 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import threading
 
-def run():
-    server = HTTPServer(("0.0.0.0", 10000), BaseHTTPRequestHandler)
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"TrpZy Bot is ONLINE")
+
+if __name__ == "__main__":
+    server = HTTPServer(("0.0.0.0", 10000), Handler)
+    print("Server running on port 10000")
     server.serve_forever()
-
-threading.Thread(target=run).start()
-
-print("Server running")
